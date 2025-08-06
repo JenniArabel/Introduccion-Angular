3069248af6dc28ce1cb2f1e778281382ce9d1712
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-06 07:42:35 -0300

    Create saludo component

---
+++ Saludos/src/app/saludo/pages/saludo-page/saludo-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "saludo-page",
+  imports: [RouterOutlet],
+  templateUrl: "./saludo-page.component.html",
+})
+export default class SaludoPageComponent {
+  
+}
